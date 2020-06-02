307e02a94561f1551e62b22920f56bb92d3815ec
= <benedict.tesha@jamaatech.com>
2020-06-02 18:07:38 +0300

    fix: stringify json before insert into db

--- routes/verify-fingerprint.js
+++ routes/verify-fingerprint.js
@@ -39,7 +39,7 @@ module.exports = (soapRequest) => {
           if(result.code === '00') {
             //Finger print match was successfull
             const json = await convertToJson(result.payload, getMapping());
-            log.responseJson = (await xmlToJson(result.payload)) || '';
+            log.responseJson = JSON.stringify(await xmlToJson(result.payload));
 
             response.json({
               id: result.id,
