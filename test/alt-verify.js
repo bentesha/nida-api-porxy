a5fb9fb7d449ad37c39d3aee25fb1aeff263b03f
= <benedict.tesha@jamaatech.com>
2020-06-23 11:32:30 +0300

    fix alt-verify test suites

--- test/alt-verify.js
+++ test/alt-verify.js
@@ -1,16 +1,25 @@
 const chai = require('chai');
 const sinon = require('sinon');
+const rijndael = require('../rijndael');
 const SoapRequest = require('../soap-request');
+const RsaCrypto  = require('../rsa-crypto');
+const SoapEndPoint = require('../soap-end-point');
 const supertest = require('supertest');
 const fs = require('fs');
 const path = require('path');
 const createApp = require('../app');
 
 const expect = chai.expect;
+const privateKey = fs.readFileSync(path.join(__dirname, '..', 'keys', 'private.key'));
+const publicKey = fs.readFileSync(path.join(__dirname, '..', 'keys', 'nida-public.cer'));
+const rsaCrypto = new RsaCrypto(privateKey, publicKey);
+const endPoint = new SoapEndPoint();
+
 
 describe('API /verify-fingerprint', () => {
   it('should return a json response', async () => {
     const soapRequest = new SoapRequest(endPoint, rsaCrypto, rijndael);
+    const responseXml = fs.readFileSync(path.join(__dirname, 'data', 'response-altverify-correct.xml'))
     endPoint.post = sinon.fake.resolves(responseXml);
 
     const data = {
